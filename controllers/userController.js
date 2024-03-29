140534347728482fcff5a574dd88351ec6a7e99f
rizkirobawa <rizkirobawa@gmail.com>
2024-03-30 04:41:05 +0700

    fixed bugs

--- controllers/userController.js
+++ controllers/userController.js
@@ -114,6 +114,7 @@ module.exports = {
       res.status(200).json({
         status: true,
         message: `User with ID ${id} deleted successfully`,
+        data: null,
       });
     } catch (error) {
       next(error);
