b8e99efd442cde2ab3a55392a0019046b8f69144
rizkirobawa <rizkirobawa@gmail.com>
2024-03-29 17:35:54 +0700

    Added API Delete and Update on Users

--- controllers/userController.js
+++ controllers/userController.js
@@ -78,7 +78,7 @@ module.exports = {
       if (!user) {
         return res.status(404).json({
           status: false,
-          message: "Can't find user with id " + id,
+          message: "Can't find user with ID " + id,
           data: null,
         });
       }
@@ -86,6 +86,82 @@ module.exports = {
       res.status(200).json({
         status: true,
         message: "OK",
+        data: user,
+      });
+    } catch (error) {
+      next(error);
+    }
+  },
+  delete: async (req, res, next) => {
+    try {
+      const id = Number(req.params.id);
+
+      const user = await prisma.user.findUnique({
+        where: { id: id },
+      });
+
+      if (!user) {
+        return res.status(404).json({
+          status: false,
+          message: `User with ID ${id} not found`,
+        });
+      }
+
+      await prisma.user.delete({
+        where: { id: id },
+      });
+
+      res.status(200).json({
+        status: true,
+        message: `User with ID ${id} deleted successfully`,
+      });
+    } catch (error) {
+      next(error);
+    }
+  },
+
+  update: async (req, res, next) => {
+    try {
+      const id = Number(req.params.id);
+      const { name, email, password, identity_type, identity_number, address } =
+        req.body;
+
+      if (!identity_number || identity_number.length !== 16) {
+        return res.status(400).json({
+          status: false,
+          message: "Invalid identity number. Must be exactly 16 characters",
+        });
+      }
+
+      const user = await prisma.user.update({
+        where: { id: id },
+        data: {
+          name: name,
+          email: email,
+          password: password,
+          profile: {
+            update: {
+              identity_type: identity_type,
+              identity_number: identity_number,
+              address: address,
+            },
+          },
+        },
+        include: {
+          profile: true,
+        },
+      });
+
+      if (!user) {
+        return res.status(404).json({
+          status: false,
+          message: `User with ID ${id} not found`,
+        });
+      }
+
+      res.status(200).json({
+        status: true,
+        message: `Update data with ID ${id} successfully`,
         data: user,
       });
     } catch (error) {
--- routes/v1/index.js
+++ routes/v1/index.js
@@ -5,5 +5,7 @@ const userController = require("../../controllers/userController");
 router.post("/users", userController.register);
 router.get("/users", userController.index);
 router.get("/users/:id", userController.show);
+router.delete("/users/:id",userController.delete)
+router.put("/users/:id",userController.update)
 
 module.exports = router;
